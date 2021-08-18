39ba82c691a3abdb1d3b2716866b9ade255837aa
June Lee <junehaklee94@gmail.com>
2021-08-18 18:38:09 -0500

    Finalized queries

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,16 @@
-CREATE TABLE IF NOT EXISTS reviews (
+drop table if exists reviews;
+
+drop table if exists reviews_photos;
+
+drop table if exists characteristics;
+
+drop table if exists characteristics_reviews;
+
+CREATE TABLE reviews (
   id SERIAL PRIMARY KEY,
   product_id int,
   rating int,
-  date timestamp,
+  date bigint,
   summary VARCHAR,
   body VARCHAR,
   recommend boolean,
@@ -10,24 +18,46 @@ CREATE TABLE IF NOT EXISTS reviews (
   reviewer_name VARCHAR,
   reviewer_email VARCHAR,
   response VARCHAR,
-  helpfulness int,
-)
+  helpfulness int
+);
 
-CREATE TABLE IF NOT EXISTS reviews_photos (
+CREATE TABLE reviews_photos (
   id SERIAL PRIMARY KEY,
-  url VARCHAR,
   review_id int,
-)
+  url VARCHAR
+);
 
-CREATE TABLE IF NOT EXISTS characteristics (
+CREATE TABLE characteristics (
   id SERIAL PRIMARY KEY,
   product_id int,
-  name VARCHAR,
-)
+  name VARCHAR
+);
 
-CREATE TABLE IF NOT EXISTS characteristics_reviews (
+CREATE TABLE characteristics_reviews (
   id SERIAL PRIMARY KEY,
   review_id int,
   characteristics_id int,
-  value int,
-)
+  value int
+);
+
+/*
+
+psql junelee -h localhost -d reviewsdata -f /Users/junelee/Reviews/database/schema.sql
+copy reviews from '/Users/junelee/reviews.csv' with delimiter ',' csv header;
+copy characteristics from '/Users/junelee/characteristics.csv' with delimiter ',' csv header;
+copy characteristics_reviews from '/Users/junelee/characteristic_reviews.csv' with delimiter ',' csv header;
+copy reviews_photos from '/Users/junelee/reviews_photos.csv' with delimiter ',' csv header;
+
+ALTER TABLE reviews
+  ALTER COLUMN date TYPE timestamp without time zone
+  USING to_timestamp(date/1000),
+  ALTER COLUMN date SET DEFAULT current_timestamp;
+
+  SELECT setval('reviews_id_seq', (select max(id) from reviews));
+  SELECT setval('reviews_photos_id_seq', (select max(id) from reviews_photos));
+  SELECT setval('characteristics_id_seq', (select max(id) from characteristics));
+  SELECT setval('characteristics_reviews_id_seq', (select max(id) from characteristics_reviews));
+
+
+
+*/
--- server.js
+++ server.js
@@ -48,22 +48,129 @@ app.get('/reviews', (req, res) => {
       "count": count,
       "results": data.rows
     }
-    console.log(resultObj)
     res.send(resultObj)
   })
 })
 
 app.get('/reviews/meta', (req, res) => {
 
-  const id = parseInt(req.query.product_id)
-  let queryString = `select rating from reviews where product_id=${id}`
-  pool.query(queryString, (err, data) => {
+  const product_id = parseInt(req.query.product_id)
+
+  const ratingQuery = `select count(rating) as "5",
+  (select count(rating) as "4" from reviews where product_id=${product_id} and rating = 4),
+  (select count(rating) as "3" from reviews where product_id=${product_id} and rating = 3),
+  (select count(rating) as "2" from reviews where product_id=${product_id} and rating = 2),
+  (select count(rating) as "1" from reviews where product_id=${product_id} and rating = 1)
+  from reviews where product_id = ${product_id} and rating = 5`;
+
+
+  const characteristicsQuery = `select name, id,
+  (select avg(value) from characteristics_reviews where characteristic_id = characteristics.id) as average from characteristics where product_id = ${product_id}`;
+
+  const recommendQuery = `select count(recommend) as true, (select count(recommend) as false from reviews where product_id = ${product_id} and recommend = false) from reviews where product_id = ${product_id} and recommend = true`;
+
+
+  pool.query(ratingQuery, (err, data) => {
+    let ratings = data.rows[0]
+    pool.query(characteristicsQuery, (err, data) => {
+      let characteristics = data.rows
+      characteristicsTransform = {};
+      characteristics.forEach((item) => {
+        characteristicsTransform[item.name] = {id: item.id, value: item.average};
+      })
+      pool.query(recommendQuery, (err, data) => {
+        let recommend = data.rows[0]
+        let result = {
+          "product_id": JSON.stringify(product_id),
+          "ratings": ratings,
+          "recommended": recommend,
+          "characteristics": characteristicsTransform
+        }
+        res.send(result)
+      })
+    })
+  })
+})
+
+app.post('/reviews', (req, res) => {
+  const product_id = parseInt(req.body.product_id)
+  const rating = req.body.rating
+  const summary = req.body.summary
+  const body = req.body.body
+  const recommend = req.body.recommend
+  const reviewer_name = req.body.reviewer_name
+  const reviewer_email = req.body.reviewer_email
+  const photos = req.body.photos
+  const characteristics = req.body.characteristics
+  const reported = req.body.report || false
+  const date = 'NOW()'
+  const response = req.body.response || null
+  const helpfulness = req.body.response || 0
+  const keys = Object.keys(characteristics)
+  const value = Object.values(characteristics)
+
+
+
+  let values = [product_id, rating, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness, photos, keys, value]
+
+
+
+  let bodyString = `with review as (
+    insert into reviews (product_id, rating, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+    returning id
+    ),
+  photo as (
+    insert into reviews_photos (review_id, url)
+    select id,
+    unnest(($11)::text[])
+    from review
+  )
+  insert into characteristics_reviews (review_id, characteristics_id, value)
+  select id,
+  unnest(($12)::int[]),
+  unnest(($13)::int[])
+  from review`
+
+
+
+
+
+  pool.query(bodyString, values)
+      .then((data) => {
+        res.status(201).send('post worked booooo')
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+})
+
+
+app.put('/reviews/:review_id/helpful', (req, res) => {
+  const review_id = req.params.review_id;
+
+  let paramString = `update reviews set helpfulness = helpfulness + 1 where id =${review_id}`;
+
+  pool.query(paramString, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
     console.log(data)
-
-
     res.send(data)
   })
+})
 
+app.put('/reviews/:review_id/report', (req, res) => {
+  const review_id = req.params.review_id;
+
+  let paramString = `update reviews set reported = true where id=${review_id}`;
+
+  pool.query(paramString, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(err);
+    res.send(data)
+  })
 })
 
 
